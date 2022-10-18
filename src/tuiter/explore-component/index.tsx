582eaddbe5cb5c4d5082a4cc802b344daa2e4722
Bill Wohlers <wohlers.w@northeastern.edu>
2022-10-18 11:56:11 -0400

    Fix

--- src/tuiter/explore-component/index.tsx
+++ src/tuiter/explore-component/index.tsx
@@ -13,28 +13,34 @@ function ExploreComponent() {
           <i
             className="bi bi-search position-absolute
                        wd-nudge-up"
-          ></i>
+          />
         </div>
         <div className="col-1">
           <i
             className="wd-bottom-4 text-primary float-end bi
                        bi-gear-fill fs-2 position-relative"
-          ></i>
+          />
         </div>
       </div>
       <ul className="nav nav-pills mb-2">
         <li className="nav-item">
-          <a className="nav-link active">For You</a>
+          <a className="nav-link active" href="#">
+            For You
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">Trending</a>
+          <a className="nav-link" href="#">
+            Trending
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">News</a>
+          <a className="nav-link" href="#">
+            News
+          </a>
         </li>
       </ul>
       <div className="position-relative mb-2">
-        <img src="/tuiter/img/starship.webp" className="w-100" />
+        <img src="/tuiter/img/starship.webp" className="w-100" alt="Starship" />
         <h1 className="position-absolute wd-nudge-up text-white">
           SpaceX Starship
         </h1>
